5c77abf94043d732b49a3a5d1b6e2f1986829bca
septaard <tmyprkr@gmail.com>
2026-06-09 00:29:00 +0700

    505

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Mengarahkan Vercel untuk menjalankan bootstrap utama Laravel
+// 1. Vercel bersifat Read-Only. Kita paksa Laravel melakukan kompilasi Blade View di folder /tmp
+$viewPath = '/tmp/storage/framework/views';
+if (!is_dir($viewPath)) {
+    mkdir($viewPath, 0755, true);
+}
+putenv("VIEW_COMPILED_PATH={$viewPath}");
+
+// 2. Otomatisasi pembuatan file database SQLite di folder /tmp jika belum ada
+$dbPath = '/tmp/database.sqlite';
+if (!file_exists($dbPath)) {
+    touch($dbPath);
+}
+
+// 3. Panggil bootstrap utama asli milik Laravel
 require __DIR__ . '/../public/index.php';
